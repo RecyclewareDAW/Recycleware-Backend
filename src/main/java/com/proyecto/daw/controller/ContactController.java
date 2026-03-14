135f42c18840070e338057464923a78c9c2250f7
Bela <belasimonyan@gmail.com>
2026-03-14 18:58:01 +0100

    Quitandoi CrossOrigin de Contacto porque tenemos un WebConfig y con eso no lo necesitamos.

--- src/main/java/com/proyecto/daw/controller/ContactController.java
+++ src/main/java/com/proyecto/daw/controller/ContactController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +20,6 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/contacto")
-@CrossOrigin(origins = "*") // Permite peticiones desde React (CORS)
 public class ContactController {
 
     @Autowired
