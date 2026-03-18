cfac0f2bfd5f4f7e394bb0eedcf4e5106a2cb9d2
LGCaparros <luicapper@alu.edu.gva.es>
2026-03-18 12:55:37 +0100

    Imagenes implementadas en data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -60,39 +60,36 @@ INSERT INTO productos(nombre, id_categoria, id_estado, id_disponibilidad, descri
 VALUES ('Ratón Mars', 1, 2, 1, 'Ratón gastado con dos botones laterales y boton de cambio de DPI', 'http://localhost:8080/api/images/6');
 
 INSERT INTO productos(nombre, id_categoria, id_estado, id_disponibilidad, descripcion, imagen_url)
-VALUES ('Ratón inalambrico Logitec', 1, 2, 1, 'Ratón inalambrico con usb independiente con 2 botones laterales y poco rango de conexión', 'https://placehold.co/600x400/green/white');
+VALUES ('Ratón inalambrico Logitec', 1, 2, 1, 'Ratón inalambrico con usb independiente con 2 botones laterales y poco rango de conexión', 'http://localhost:8080/api/images/5');
 
     
     -- Sobremesa
 INSERT INTO productos(nombre, id_categoria, id_estado, id_disponibilidad, descripcion, imagen_url)
 VALUES ('Torre Artec netanya', 3, 1, 1, 'Core I5 8RAM 1TB hhd y lector de Dvd', 'https://thumb.pccomponentes.com/w-530-530/articles/1083/10837879/1830-pcgam-oficina-madrid-slim-intel-core-i3-10100-16gb-500gb-ssd.jpg');
 
-INSERT INTO productos(nombre, id_categoria, id_estado, id_disponibilidad, descripcion, imagen_url)
-VALUES ('Torre dañada Nox', 3, 4, 1, 'Torre rota que se desconoce por que no arranca y antiguo', 'https://placehold.co/400x600/red/black');
-
     
     -- Componentes
 INSERT INTO productos(nombre, id_categoria, id_estado, id_disponibilidad, descripcion, imagen_url)
-VALUES ('Procesador Intel i7-6700K', 4, 1, 2, 'Procesador con apenas uso', 'https://placehold.co/600x600/blue/white');
+VALUES ('Procesador Intel i7-6700K', 4, 1, 2, 'Procesador con apenas uso', 'http://localhost:8080/api/images/7');
 
 INSERT INTO productos(nombre, id_categoria, id_estado, id_disponibilidad, descripcion, imagen_url)
-VALUES ('GPU Sapphire Radeon HD 5570', 4, 2, 1, 'Tarjeta gráfica con varios años de uso pero todavía funcional ni indicios de estropearse', 'https://placehold.co/600x400/red/black');
+VALUES ('GPU Sapphire Radeon HD 5570', 4, 2, 1, 'Tarjeta gráfica con varios años de uso pero todavía funcional ni indicios de estropearse', 'http://localhost:8080/api/images/7');
 
     
     -- Monitores
 INSERT INTO productos(nombre, id_categoria, id_estado, id_disponibilidad, descripcion, imagen_url)
-VALUES ('Monitor Dell P2212', 5, 1, 1, 'Monitor Dell de una oficinia que pese al uso esta en buen estado todavía', 'https://placehold.co/600x600/blue/white');
+VALUES ('Monitor Dell P2212', 5, 1, 1, 'Monitor Dell de una oficinia que pese al uso esta en buen estado todavía', 'http://localhost:8080/api/images/2');
 
 INSERT INTO productos(nombre, id_categoria, id_estado, id_disponibilidad, descripcion, imagen_url)
-VALUES ('Monitor VSDISPLAY LCD', 5, 3, 1, 'Monitor con 3 franjas verticales negras pero todavia funcional', 'https://placehold.co/600x400/red/black');
+VALUES ('Monitor VSDISPLAY LCD', 5, 3, 1, 'Monitor con 3 franjas verticales negras pero todavia funcional', 'http://localhost:8080/api/images/1');
 
     
     -- Portatiles
 INSERT INTO productos(nombre, id_categoria, id_estado, id_disponibilidad, descripcion, imagen_url)
-VALUES ('Dell Inspiron', 2, 3, 1, '8Gb Ram 120Gb Hdd. Pantalla dañada por un golpe pero el resto del dispositivo funciona correctamente', 'https://placehold.co/600x600/blue/white');
+VALUES ('Dell Inspiron', 2, 3, 1, '8Gb Ram 120Gb Hdd. Pantalla dañada por un golpe pero el resto del dispositivo funciona correctamente', 'http://localhost:8080/api/images/3');
 
 INSERT INTO productos(nombre, id_categoria, id_estado, id_disponibilidad, descripcion, imagen_url)
-VALUES ('Lenovo G500s', 2, 1, 1, '8Gb Ram 320Gb Hdd y webcam integrada', 'https://placehold.co/600x400/red/black');
+VALUES ('Lenovo G500s', 2, 1, 1, '8Gb Ram 320Gb Hdd y webcam integrada', 'http://localhost:8080/api/images/4');
 
 
 
